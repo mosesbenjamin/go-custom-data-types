15f284cdd22df3bbe7ced3e086ad590b5569e086
mosesbenjamin <mavewreek@gmail.com>
2022-06-19 07:31:36 -0500

    changing the state of the struct

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import "datatypes/organization"
+import (
+	"datatypes/organization"
+	"fmt"
+)
 
 func main() {
 	p := organization.NewPerson("Collin", "Milner")
+	err := p.SetTwitterHandler("@morrison")
+	if err != nil {
+		fmt.Printf("An error occurred setting twitter handler: %s", err.Error())
+	}
 	println(p.ID())
 	println(p.FullName())
 }
